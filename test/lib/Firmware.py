b4273171beabd10e3566ffe0ec6564b1c37a8e42
Phan Linh <phanlinh86@gmail.com>
2025-01-28 18:51:35 +0700

    EMBEDDED-44 EMBEDDED-21 EMBEDDED-23 <Update firmware test lib with release and binary comparison function>

--- test/lib/Firmware.py
+++ test/lib/Firmware.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
+import filecmp
 
 class Firmware(object):
+    DEFAULT_REV = "8a00"
     def __init__(self):
         self.main_path = '\\'.join(os.path.realpath(__file__).split('\\')[:-3])  # Main path of the whole project including firmware, test, etc.
         self.build_path = self.main_path + "\\build"                # Path of the test folder
@@ -61,24 +63,72 @@ class Firmware(object):
         return macros
 
     def build(self, build_option="blink_a"):
-        subprocess.run(["make ", build_option], cwd=self.main_path)
+        subprocess.run("make " + build_option, cwd=self.main_path)
+
+    def release(self, build_option="blink_a", commit_id=None):
+        # Check out to a specific commit
+        if commit_id is not None:
+            self.git_checkout(commit_id)
+        # Release the firmware with zip file
+        subprocess.run("make " + build_option + f" rel=3 rev={self.DEFAULT_REV}", cwd=self.main_path)
+        # Get commit ID from GIT
+        commit_id = self.git_info()['id']
+        # Rename the zip file by build option and commit ID
+        zip_file = self.main_path + f"\\release\\{build_option}\\{build_option}_{commit_id}.zip"
+        if os.path.exists(zip_file):
+            os.remove(zip_file)
+        os.rename(self.main_path + f"\\release\\{build_option}_{self.DEFAULT_REV}.zip", zip_file)
+        # Remove the old build folder specied by {self.DEFAULT_REV}
+        subprocess.run(f"rmdir /s /q {self.main_path}\\release\\{build_option}\\{self.DEFAULT_REV}", shell=True)
+
+    def compare(self, build_option="blink_a", commit_id1=None, commit_id2=None):
+        # Create a temporary folder to store the main.elf files
+        os.makedirs(self.main_path + "\\build\\compare", exist_ok=True)
+        # Check out to a specific commit
+        if commit_id1 is not None:
+            self.git_checkout(commit_id1)
+        else:
+            # Checkout to the latest commit
+            self.git_checkout()
+        # Build the firmware with the first commit ID
+        self.build(build_option)
+        # Temporary replace main.elf to main1.elf
+        if os.path.exists(self.main_path + "\\build\\compare\\main1.elf"):
+            os.remove(self.main_path + "\\build\\compare\\main1.elf")
+        os.rename(self.main_path + "\\build\\main.elf", self.main_path + "\\build\\compare\\main1.elf")
+        # Check out to a specific commit
+        if commit_id2 is not None:
+            self.git_checkout(commit_id2)
+        else:
+            # Checkout the 2nd latest commit
+            commit_id2 = os.popen('git log --pretty=format:"%H"').read().strip().splitlines()[1]
+            self.git_checkout(commit_id2)
+        # Build the firmware with the second commit ID
+        self.build(build_option)
+        # Temporary replace main.elf to main2.elf
+        if os.path.exists(self.main_path + "\\build\\compare\\main2.elf"):
+            os.remove(self.main_path + "\\build\\compare\\main2.elf")
+        os.rename(self.main_path + "\\build\\main.elf", self.main_path + "\\build\\compare\\main2.elf")
+        # Compare the two main.elf files
+        print(f"Compare the two main.elf files of commit ID: {commit_id1} vs {commit_id2}")
+        result = filecmp.cmp(f"{self.main_path}\\build\\compare\\main1.elf", f"{self.main_path}\\build\\compare\\main2.elf")
+        # Remove the temporary folder
+        subprocess.run(f"rmdir /s /q {self.main_path}\\build\\compare", shell=True)
+        # Checkout back to master
+        self.git_checkout()
+        return result
 
     def clean(self):
         subprocess.run(["make clean"], cwd=self.main_path)
 
     def download(self, flash_option="flash_a"):
-        subprocess.run(["make ", flash_option], cwd=self.main_path)
+        subprocess.run("make " + flash_option, cwd=self.main_path)
 
     @staticmethod
     def git_info(print_info=False):
         # Get information from GIT
         info = os.popen('git log -1 --pretty=format:"%H%n%an%n%ae%n%ad%n%s"').read().strip().splitlines()
-        info_dict = {}
-        info_dict['id']         = info[0]
-        info_dict['author']     = info[1]
-        info_dict['email']      = info[2]
-        info_dict['date']       = info[3]
-        info_dict['message']    = info[4]
+        info_dict = {'id': info[0], 'author': info[1], 'email': info[2], 'date': info[3], 'message': info[4]}
         # Print out git information
         if print_info:
             print("Git information -----------------------------------------")
@@ -94,7 +144,7 @@ class Firmware(object):
         # Checkout to a specific commit
         # If commit_id is not provided, checkout latest commit
         if commit_id:
-            subprocess.run(["git checkout ", commit_id], cwd=self.main_path)
+            subprocess.run("git checkout " + commit_id, cwd=self.main_path)
         else:
             subprocess.run("git checkout master", cwd=self.main_path)
 
@@ -119,7 +169,10 @@ if __name__ == "__main__":
     firmware = Firmware()
     firmware.git_info(print_info=True)
     firmware.git_checkout(commit_id=None)   # Checkout to the latest commit
+    # Compare same latest commits
+    print("Binary compare result: ", firmware.compare("blink_a"))
     firmware.build("blink_a")
-    firmware.download("flash_a")
+    # firmware.download("flash_c")
+    firmware.release("blink_a")
     firmware.readmacro()
     print("Firmware test passed")
